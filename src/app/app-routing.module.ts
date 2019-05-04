b073f9dcabe4e72225b47480cbdbc1cef3c63c0c
jtc10005 <jtc10005@gmail.com>
2019-05-04 13:09:48 -0500

    Adding material components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [];
+const homeRoute: Route = {
+  path: '/',
+  component: HomeComponent
+};
+
+const listRoute: Route = {
+  path: '/list',
+  component: ListComponent
+};
+
+const routes: Routes = [homeRoute, listRoute];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { AppMaterialModule } from './app-material/app-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
