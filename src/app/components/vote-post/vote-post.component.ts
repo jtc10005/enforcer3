37a7279e59342a047c2181dcefce139ed2e7ee89
jtc10005 <jtc10005@gmail.com>
2019-05-27 22:28:32 -0500

    voting fixed

--- src/app/components/vote-post/vote-post.component.ts
+++ src/app/components/vote-post/vote-post.component.ts
@@ -17,11 +17,21 @@ export class VotePostComponent implements OnInit {
 
   constructor(private as: AppService) { }
 
+  get CanUpVote() {
+    return !this.alreadyVotedUp && this.post.votes.filter(x => x.UserId === this.as.userId && x.VoteType === VoteType.UP).length !== 0;
+  }
+
+  get canDownVote() {
+    return !this.alreadyVotedDown && this.post.votes.filter(x => x.UserId === this.as.userId && x.VoteType === VoteType.DOWN).length !== 0;
+  }
+
   ngOnInit() {
     if (this.post && !this.post.votes) {
       this.post.votes = [];
     }
   }
+
+
   vote(vote: number) {
     if (this.post.trueCount === 0 && vote === -1) {
       return;
@@ -29,8 +39,15 @@ export class VotePostComponent implements OnInit {
     this.alreadyVotedUp = vote === 1;
     this.alreadyVotedDown = vote === -1;
     // this.post.trueCount += vote;
+    this.removeExistingVotesFromVotes();
     this.post.votes.push(new VoteItem({ VoteType: vote === 1 ? VoteType.UP : VoteType.DOWN, UserId: this.as.userId }));
-    // this.post.calculateVoteTotal();
+
+
     this.as.updatePost(this.post);
+  }
+  removeExistingVotesFromVotes() {
+    if (this.post && this.post.votes) {
+      this.post.votes = this.post.votes.filter(x => x.UserId !== this.as.userId);
+    }
   }
 }
